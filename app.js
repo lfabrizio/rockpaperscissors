e21429c6d1e5ec7b0cfcb7d3bd6f0d2da586d2e4
lfabrizio <louisfabrizio.student@careerdevs.com>
2019-09-25 21:02:39 -0400

    33

---
+++ app.js
@@ -0,0 +1,11 @@
+const userScore = 0;
+const computerScore = 0;
+const userScore_span = document.getElementById("user-score"); // to show its in a span tag
+const computerScore_span = document.getElementById("computer-score"); // and DOM variable
+const scoreBoard_div = document.querySelector(".score-board"); // to show its in a div tag
+const result_div = document.querySelector(".result");
+const rock_div = document.getElementById("r"); // get the button rock
+const paper_div = document.getElementById("p"); // get the button paper
+const scissors_div = document.getElementById("s"); // get the button scissors
+// now we have cached the DOM ( storing for future use)
+
